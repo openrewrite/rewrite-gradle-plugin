f39184daa9febf48bdd015525f833d973225cc2f
Jon Schneider <jschneider@gradle.com>
2020-03-31 21:32:56 -0400

    Remove Java 8 check

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,11 +13,3 @@ group = "org.gradle.rewrite"
 description = "Automatically refactor source"
 
 evaluationDependsOn("plugin")
-
-tasks.named("releaseCheck") {
-    doFirst {
-        if(!JavaVersion.current().isJava8) {
-            throw GradleException("Plugin releases should use Java 8.")
-        }
-    }
-}
