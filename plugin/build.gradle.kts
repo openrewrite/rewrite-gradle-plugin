09475c6ba882aa46381a1792b657ce8d2adc12d8
Jon Schneider <jschneider@gradle.com>
2020-03-31 22:19:52 -0400

    Don't shade for now

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,4 +1,3 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.gradle.rewrite.build.GradleVersionData
 import org.gradle.rewrite.build.GradleVersionsCommandLineArgumentProvider
 import java.net.URI
@@ -46,26 +45,12 @@ dependencies {
 
     api("org.gradle.rewrite:rewrite-java:latest.integration")
     api("org.gradle.rewrite.plan:rewrite-checkstyle:latest.integration")
-    implementation("org.eclipse.jgit:org.eclipse.jgit:latest.release")
+    api("org.eclipse.jgit:org.eclipse.jgit:latest.release")
 
     testImplementation(gradleTestKit())
     testImplementation("org.codehaus.groovy:groovy-all:2.5.8")
     testImplementation("org.spockframework:spock-core:1.3-groovy-2.5")
 }
-
-val shadowJar = tasks.named<ShadowJar>("shadowJar")
-shadowJar.configure {
-    configurations = listOf(plugin)
-    dependencies {
-        include(dependency("org.eclipse.jgit:org.eclipse.jgit"))
-    }
-    relocate("org.eclipse.jgit", "org.gradle.rewrite.gradleplugin.org.eclipse.jgit")
-    @Suppress("DEPRECATION")
-    classifier = ""
-}
-
-tasks.getByName("jar").enabled = false
-tasks.getByName("jar").dependsOn(shadowJar)
 
 tasks.pluginUnderTestMetadata {
     pluginClasspath.from(plugin)
@@ -75,9 +60,7 @@ publishing {
     publications {
         create<MavenPublication>("plugin") {
             artifactId = "rewrite-gradle-plugin"
-            artifact(shadowJar.get()) {
-                classifier = null
-            }
+            artifact(tasks.named<Jar>("jar").get())
         }
     }
     repositories {
