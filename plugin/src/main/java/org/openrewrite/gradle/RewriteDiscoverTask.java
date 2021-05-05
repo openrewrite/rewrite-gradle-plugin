43b17b114fc054618b2d644fadd0b03e5d6aef0b
Aaron Gershman <5619476+aegershman@users.noreply.github.com>
2021-05-05 14:20:47 -0500

    RewriteDiscover categorized recipes available on classpath (#49)

--- plugin/src/main/java/org/openrewrite/gradle/RewriteDiscoverTask.java
+++ plugin/src/main/java/org/openrewrite/gradle/RewriteDiscoverTask.java
@@ -16,11 +16,18 @@
 package org.openrewrite.gradle;
 
 import org.gradle.api.artifacts.Configuration;
+import org.gradle.api.internal.tasks.userinput.UserInputHandler;
 import org.gradle.api.logging.Logger;
 import org.gradle.api.logging.Logging;
+import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.SourceSet;
 import org.gradle.api.tasks.TaskAction;
-import org.openrewrite.gradle.RewriteReflectiveFacade.*;
+import org.gradle.api.tasks.options.Option;
+import org.openrewrite.gradle.RewriteReflectiveFacade.Environment;
+import org.openrewrite.gradle.RewriteReflectiveFacade.NamedStyles;
+import org.openrewrite.gradle.RewriteReflectiveFacade.OptionDescriptor;
+import org.openrewrite.gradle.RewriteReflectiveFacade.RecipeDescriptor;
+import org.openrewrite.gradle.ui.RecipeDescriptorTreePrompter;
 
 import javax.inject.Inject;
 import java.util.Collection;
@@ -28,6 +35,17 @@ import java.util.Set;
 
 public class RewriteDiscoverTask extends AbstractRewriteTask {
     private static final Logger log = Logging.getLogger(RewriteDiscoverTask.class);
+    private boolean interactive;
+
+    @Option(description = "Whether to enter an interactive shell to explore available recipes.", option = "interactive")
+    public void setInteractive(boolean interactive) {
+        this.interactive = interactive;
+    }
+
+    @Input
+    public boolean isInteractive() {
+        return this.interactive;
+    }
 
     @Override
     protected Logger getLog() {
@@ -45,32 +63,63 @@ public class RewriteDiscoverTask extends AbstractRewriteTask {
     public void run() {
         Environment env = environment();
         Collection<RecipeDescriptor> availableRecipeDescriptors = env.listRecipeDescriptors();
-        Set<String> activeRecipes = getActiveRecipes();
-        Collection<NamedStyles> availableStyles = env.listStyles();
-        Set<String> activeStyles = getActiveStyles();
 
-        log.quiet("Available Recipes:");
-        for (RecipeDescriptor recipe : availableRecipeDescriptors) {
-            log.quiet("\tname: " + recipe.getName());
+        if (interactive) {
+            log.quiet("Entering interactive mode, Ctrl-C to exit...");
+            UserInputHandler prompter = getServices().get(UserInputHandler.class);
+            RecipeDescriptorTreePrompter treePrompter = new RecipeDescriptorTreePrompter(prompter);
+            RecipeDescriptor rd = treePrompter.execute(availableRecipeDescriptors);
+            writeRecipeDescriptor(rd, true, 0);
+        } else {
+            Set<String> activeRecipes = getActiveRecipes();
+            Collection<NamedStyles> availableStyles = env.listStyles();
+            Set<String> activeStyles = getActiveStyles();
+
+            log.quiet("Available Recipes:");
+            for (RecipeDescriptor recipe : availableRecipeDescriptors) {
+                log.quiet("\tname: " + recipe.getName());
+            }
+
+            log.quiet("Available Styles:");
+            for (NamedStyles style : availableStyles) {
+                log.quiet("\tname: " + style.getName());
+            }
+
+            log.quiet("Active Styles:");
+            for (String style : activeStyles) {
+                log.quiet("\tname: " + style);
+            }
+
+            log.quiet("Active Recipes:");
+            for (String activeRecipe : activeRecipes) {
+                log.quiet("\tname: " + activeRecipe);
+            }
+
+            log.quiet("Found " + availableRecipeDescriptors.size() + " available recipes and " + availableStyles.size() + " available styles.");
+            log.quiet("Configured with " + activeRecipes.size() + " active recipes and " + activeStyles.size() + " active styles.");
         }
-
-        log.quiet("Available Styles:");
-        for (NamedStyles style : availableStyles) {
-            log.quiet("\tname: " + style.getName());
-        }
-
-        log.quiet("Active Styles:");
-        for (String style : activeStyles) {
-            log.quiet("\tname: " + style);
-        }
-
-        log.quiet("Active Recipes:");
-        for (String activeRecipe : activeRecipes) {
-            log.quiet("\tname: " + activeRecipe);
-        }
-
-        log.quiet("Found " + availableRecipeDescriptors.size() + " available recipes and " + availableStyles.size() + " available styles.");
-        log.quiet("Configured with " + activeRecipes.size() + " active recipes and " + activeStyles.size() + " active styles.");
 
     }
+
+    private void writeRecipeDescriptor(RecipeDescriptor rd, boolean verbose, int indentLevel) {
+        log.quiet(indent(indentLevel, "name: " + rd.getName()));
+        if (verbose) {
+            log.quiet(indent(indentLevel, "displayName: " + rd.getDisplayName()));
+            log.quiet(indent(indentLevel, "description: " + rd.getDescription()));
+
+            log.quiet(indent(indentLevel, "options: " + (rd.getOptions().isEmpty() ? "[]" : "")));
+            for (OptionDescriptor od : rd.getOptions()) {
+                log.quiet(indent(indentLevel + 1, od.getName() + ": " + od.getType() + (od.isRequired() ? "!" : "")));
+                log.quiet(indent(indentLevel + 2, "displayName: " + od.getDisplayName()));
+                log.quiet(indent(indentLevel + 2, "description: " + od.getDescription()));
+                log.quiet(indent(indentLevel + 2, (od.getExample() == null ? "" : "example: " + od.getExample())));
+            }
+        }
+
+        if (verbose) {
+            log.quiet(indent(indentLevel, ""));
+        }
+
+    }
+
 }
