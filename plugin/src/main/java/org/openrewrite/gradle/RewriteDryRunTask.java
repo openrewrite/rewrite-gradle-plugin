5c23d518d7d46ceef262e72797a8e3648edabd5d
Aaron Gershman <aegershman@gmail.com>
2021-05-05 17:01:02 -0500

    set default dryRun result be to not cache results

--- plugin/src/main/java/org/openrewrite/gradle/RewriteDryRunTask.java
+++ plugin/src/main/java/org/openrewrite/gradle/RewriteDryRunTask.java
@@ -19,6 +19,7 @@ import org.gradle.api.GradleException;
 import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.logging.Logger;
 import org.gradle.api.logging.Logging;
+import org.gradle.api.specs.Specs;
 import org.gradle.api.tasks.OutputFile;
 import org.gradle.api.tasks.SourceSet;
 import org.gradle.api.tasks.TaskAction;
@@ -44,7 +45,7 @@ public class RewriteDryRunTask extends AbstractRewriteTask {
         super(configuration, sourceSet, extension);
         setGroup("rewrite");
         setDescription("Dry run the active refactoring recipes to sources within the " + sourceSet.getName() + " SourceSet. No results will be made.");
-        getOutputs().upToDateWhen(task -> !extension.getFailOnDryRunResults());
+        getOutputs().upToDateWhen(Specs.SATISFIES_NONE);
     }
 
     @Override
