34d8edf2e3c028fbcef7a60dc5f4f942120393ab
Aaron Gershman <5619476+aegershman@users.noreply.github.com>
2022-01-21 07:01:30 -0600

    ci: dependency-check workflow on gradle-plugin (#93)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("nebula.release") version "15.3.1"
     id("io.github.gradle-nexus.publish-plugin") version "1.0.0"
+    id("org.owasp.dependencycheck") version "6.5.3" apply false
 }
 
 configure<nebula.plugin.release.git.base.ReleasePluginExtension> {
@@ -8,8 +9,15 @@ configure<nebula.plugin.release.git.base.ReleasePluginExtension> {
 }
 
 allprojects {
+    apply(plugin = "org.owasp.dependencycheck")
     group = "org.openrewrite"
     description = "Eliminate Tech-Debt. At build time."
+
+    configure<org.owasp.dependencycheck.gradle.extension.DependencyCheckExtension> {
+        analyzers.assemblyEnabled = false
+        failBuildOnCVSS = 9.0F
+    }
+
 }
 
 nexusPublishing {
