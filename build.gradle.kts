b0a0188f8da5eaa18318e879a373b9241ffa5a01
Patrick <patway99@gmail.com>
2022-04-22 11:39:16 -0700

    Update nebula-release and dependencycheck plugin versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("nebula.release") version "15.3.1"
+    id("nebula.release") version "16.0.0"
     id("io.github.gradle-nexus.publish-plugin") version "1.0.0"
-    id("org.owasp.dependencycheck") version "6.5.3" apply false
+    id("org.owasp.dependencycheck") version "7.0.4.1" apply false
 }
 
 configure<nebula.plugin.release.git.base.ReleasePluginExtension> {
