f01345d0c2b070905aa785a083811dca62661ea1
Sam Snyder <sam@moderne.io>
2022-12-09 16:47:27 -0800

    Dockerfile plain text mask to match the other plain text masks

--- plugin/src/main/java/org/openrewrite/gradle/RewriteExtension.java
+++ plugin/src/main/java/org/openrewrite/gradle/RewriteExtension.java
@@ -281,7 +281,7 @@ public class RewriteExtension {
                     "**.txt",
                     "**.jsp",
                     "**.sql",
-                    "Dockerfile"
+                    "**Dockerfile"
             ));
         }
         return plainTextMasks;
