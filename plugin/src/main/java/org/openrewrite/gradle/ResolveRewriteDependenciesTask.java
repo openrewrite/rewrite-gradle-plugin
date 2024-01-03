276ac9c4ff8fd53d3f818127dd8870d10d054097
Tim te Beek <tim@moderne.io>
2024-01-03 13:58:59 +0100

    Add rewrite-java-21 to resolved dependencies

--- plugin/src/main/java/org/openrewrite/gradle/ResolveRewriteDependenciesTask.java
+++ plugin/src/main/java/org/openrewrite/gradle/ResolveRewriteDependenciesTask.java
@@ -66,6 +66,7 @@ public class ResolveRewriteDependenciesTask extends DefaultTask {
                     deps.create("org.openrewrite:rewrite-json:" + rewriteVersion),
                     deps.create("org.openrewrite:rewrite-kotlin:" + extension.getRewriteKotlinVersion()),
                     deps.create("org.openrewrite:rewrite-java:" + rewriteVersion),
+                    deps.create("org.openrewrite:rewrite-java-21:" + rewriteVersion),
                     deps.create("org.openrewrite:rewrite-java-17:" + rewriteVersion),
                     deps.create("org.openrewrite:rewrite-java-11:" + rewriteVersion),
                     deps.create("org.openrewrite:rewrite-java-8:" + rewriteVersion),
