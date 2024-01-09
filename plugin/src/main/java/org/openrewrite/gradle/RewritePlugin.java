2a9b941ab800bb6f7ce39f917507f10e3911d4c7
Tim te Beek <tim@moderne.io>
2024-01-09 11:26:32 +0100

    Remove unused imports

--- plugin/src/main/java/org/openrewrite/gradle/RewritePlugin.java
+++ plugin/src/main/java/org/openrewrite/gradle/RewritePlugin.java
@@ -19,8 +19,6 @@ import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.Task;
 import org.gradle.api.artifacts.Configuration;
-import org.gradle.api.artifacts.dsl.RepositoryHandler;
-import org.gradle.api.artifacts.repositories.MavenArtifactRepository;
 import org.gradle.api.plugins.JavaBasePlugin;
 import org.gradle.api.plugins.JavaPluginConvention;
 import org.gradle.api.plugins.quality.CheckstyleExtension;
