3aa7aebd5a679dbd13eef24fcde47ebb553993cf
Tim te Beek <timtebeek@gmail.com>
2024-02-05 09:08:35 +0000

    refactor: Remove `test` prefix from JUnit 5 tests
    Use this link to re-run the recipe: https://app.moderne.io/recipes/org.openrewrite.java.testing.cleanup.RemoveTestPrefix?organizationId=T3BlblJld3JpdGU%3D
    
    Co-authored-by: Moderne <team@moderne.io>

--- plugin/src/test/kotlin/org/openrewrite/gradle/RewriteDryRunTest.kt
+++ plugin/src/test/kotlin/org/openrewrite/gradle/RewriteDryRunTest.kt
@@ -126,7 +126,7 @@ class RewriteDryRunTest : RewritePluginTest {
 
     @DisabledIf("lessThanGradle6_1")
     @Test
-    fun testMultiplatform(@TempDir projectDir: File) {
+    fun multiplatform(@TempDir projectDir: File) {
         gradleProject(projectDir) { 
             buildGradle("""
                 plugins {
