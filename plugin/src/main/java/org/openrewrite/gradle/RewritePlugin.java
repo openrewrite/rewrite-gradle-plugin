c87161525021b64fda2f102553100043092bba79
Tim te Beek <tim@moderne.io>
2024-08-03 12:23:58 +0200

    Add `rewrite-java-21` to `knownRewriteDependencies`

--- plugin/src/main/java/org/openrewrite/gradle/RewritePlugin.java
+++ plugin/src/main/java/org/openrewrite/gradle/RewritePlugin.java
@@ -197,6 +197,7 @@ public class RewritePlugin implements Plugin<Project> {
                 deps.create("org.openrewrite:rewrite-json:" + rewriteVersion),
                 deps.create("org.openrewrite:rewrite-kotlin:" + extension.getRewriteKotlinVersion()),
                 deps.create("org.openrewrite:rewrite-java:" + rewriteVersion),
+                deps.create("org.openrewrite:rewrite-java-21:" + rewriteVersion),
                 deps.create("org.openrewrite:rewrite-java-17:" + rewriteVersion),
                 deps.create("org.openrewrite:rewrite-java-11:" + rewriteVersion),
                 deps.create("org.openrewrite:rewrite-java-8:" + rewriteVersion),
