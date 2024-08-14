b292630ff99528ab41e4b108cbcfc2631cd9d3bb
Jonathan Schnéider <jkschneider@gmail.com>
2024-08-14 22:12:02 +0000

    refactor: Migrate to JSpecify
    Use this link to re-run the recipe: https://app.moderne.io/builder/o7lzAUcMZ?organizationId=T3BlblJld3JpdGU%3D
    
    Co-authored-by: Moderne <team@moderne.io>

--- plugin/src/main/java/org/openrewrite/gradle/isolated/ResultsContainer.java
+++ plugin/src/main/java/org/openrewrite/gradle/isolated/ResultsContainer.java
@@ -15,8 +15,8 @@
  */
 package org.openrewrite.gradle.isolated;
 
+import org.jspecify.annotations.Nullable;
 import org.openrewrite.*;
-import org.openrewrite.internal.lang.Nullable;
 import org.openrewrite.marker.Marker;
 import org.openrewrite.marker.Markers;
 import org.openrewrite.marker.Markup;
