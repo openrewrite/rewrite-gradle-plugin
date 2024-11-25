45d2474116dd1751eedc0c5b49fb9ea9311540be
Tim te Beek <timtebeek@gmail.com>
2024-11-25 09:49:18 +0000

    refactor: OpenRewrite best practices
    Use this link to re-run the recipe: https://app.moderne.io/recipes/org.openrewrite.recipes.OpenRewriteBestPractices?organizationId=T3BlblJld3JpdGU%3D
    
    Co-authored-by: Moderne <team@moderne.io>

--- plugin/src/main/java/org/openrewrite/gradle/RewritePlugin.java
+++ plugin/src/main/java/org/openrewrite/gradle/RewritePlugin.java
@@ -20,7 +20,6 @@ import org.gradle.api.Project;
 import org.gradle.api.Task;
 import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.artifacts.Dependency;
-import org.gradle.api.artifacts.DependencySet;
 import org.gradle.api.artifacts.dsl.DependencyHandler;
 import org.gradle.api.attributes.*;
 import org.gradle.api.attributes.java.TargetJvmEnvironment;
